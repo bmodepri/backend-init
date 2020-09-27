93bae6023acfe0924b665c196e40aebfe177ff64
bmodepri <garcia.priscila01@gmail.com>
2020-09-27 19:31:13 +0200

    image handling to add avatars

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
-const usersRouter = Router();
+import ensureAuthenticate from '../middlewares/ensureAuthenticated';
 
+const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     
@@ -25,5 +29,9 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: err.message })
     }
 });
+
+usersRouter.patch('/avatar', ensureAuthenticate, upload.single('avatar'), async (request, response) => {
+    return response.json({ ok: true });
+}) 
 
 export default usersRouter;
